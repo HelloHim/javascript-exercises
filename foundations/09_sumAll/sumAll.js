f9e6fa78dcd62d49ca6a1f6f36284d771160b583
hellohim <elohim.mbu@gmail.com>
2026-02-10 16:21:12 +0000

    feat: complete 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    counter = 0;
+    if ((a < 0) || (b < 0)) return "ERROR";
+    if ((Number.isInteger(a) == false) || (Number.isInteger(b) == false)) return "ERROR";
+    if (a < b) {
+        for (i = a; i <= b; i++) {
+            counter += i;
+        }
+    } else{
+        for (i = b; i <= a; i++) {
+            counter += i;
+        }
+    }
+    return counter;
 };
 
 // Do not edit below this line
