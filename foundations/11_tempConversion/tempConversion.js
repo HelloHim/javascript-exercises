9a25cf6171a9de06f8dd2c5bf08716bc6d54b2bc
hellohim <elohim.mbu@gmail.com>
2026-02-10 19:02:06 +0000

    feat: complete 11_tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitNum) {
+  let celsiusNum = ((fahrenheitNum - 32) / (1.8))
+  return Math.round(celsiusNum * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusNum) {
+  let fahrenheitNum = ((celsiusNum * 1.8) + (32))
+  return Math.round(fahrenheitNum * 10) / 10;
 };
 
 // Do not edit below this line
