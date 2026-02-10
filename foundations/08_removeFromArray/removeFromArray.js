aa4988095ee8d41ebf839f6a2d29305b570d01e0
hellohim <elohim.mbu@gmail.com>
2026-02-10 16:00:52 +0000

    feat: complete 08_removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...arrOfArgs) {
+    filteredArray = [];
+    for (i = 0; i < arr.length; i++) {
+        sameElementfound = false;
+        for (j = 0; j < arrOfArgs.length; j++) {
+            if (arr[i] === arrOfArgs[j]) {
+                sameElementfound = true;
+            }
+        }
+        if (sameElementfound == false) {
+            filteredArray.push(arr[i]);
+        }
+    }
+    console.log(filteredArray);
+    return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
