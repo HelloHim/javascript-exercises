57797d5cb0ee6f0092eebb7862d7ea7f2a7b76bd
HelloHim <elohim.mbu@gmail.com>
2026-02-19 17:24:01 +0000

    complete 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,36 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    x = Number(x);
+
+    if (x < 0) return "OOPS";
+
+    switch (x) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 1;
+            break;
+        case 2:
+            return 1;
+            break;
+        case 3:
+            return 2;
+        case false:
+            return "OOPS";
+    }
+
+    let a = 1;
+    let b = 2;
+    let c = 3;
+
+    for (i = 3; i < x; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    
+    return c;
 };
 
 // Do not edit below this line
