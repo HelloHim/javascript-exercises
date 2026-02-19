bfbd621301f53daa6f56f29bc85aa91bd14561cd
HelloHim <elohim.mbu@gmail.com>
2026-02-19 16:23:12 +0000

    complete 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanString = str.replace(/[\p{P}\p{S}\s]/gu, '');
+    strArr = cleanString.toLowerCase().split("");
+    return strArr.reduce((accumulator, item, index, array) => {
+        if (accumulator == false) return false;
+        if (item !== array.at(accumulator)) {
+            return false;
+        } else {
+            accumulator--
+            if (accumulator + array.length == -1) {
+                return true;
+            } else {
+                return accumulator
+            }
+        }
+    }, -1)
 };
 
 // Do not edit below this line
