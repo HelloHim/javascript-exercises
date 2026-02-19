06ac9d930ad5cb50751660f9526133bc269d109c
HelloHim <elohim.mbu@gmail.com>
2026-02-19 14:27:03 +0000

    complete 12_calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, item, index) => {
+    accumulator += item;
+    return accumulator;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, item, index) => {
+    if (index !== 0) {
+      accumulator *= item;
+    }
+    return accumulator;
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x == 0) {
+    return 1
+  } else {
+    const wholeNumbersBelowX = []
+    for (i = 1; i <= x; i++) {
+      wholeNumbersBelowX.push(i);
+    }
+    return wholeNumbersBelowX.reduce((accumulator, item, index) => {
+      if (index !== 0) {
+        accumulator *= item;
+      }
+      return accumulator;
+    })
+  }
 };
 
 // Do not edit below this line
