69ef1ff9340187ba8177d3da795b6a28724498a5
HelloHim <elohim.mbu@gmail.com>
2026-02-20 17:29:15 +0000

    complete 16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    oldestAge = arr.reduce((accumulator, item, index, array) => {
+        if (item.yearOfDeath == undefined) {
+            console.log(item.yearOfDeath);
+            item.yearOfDeath = currentYear;
+            console.log(item.yearOfDeath);
+        }
+        let age = item.yearOfDeath - item.yearOfBirth;
+        if (age > accumulator) {
+            accumulator = age;
+        }
+        return accumulator;
+    }, 0)
 
+    oldestPersonIndex = arr.findIndex((item, index, array) => {
+        if (item.yearOfDeath - item.yearOfBirth == oldestAge) {
+            return true;
+        } else return false;
+    })
+
+    return arr[oldestPersonIndex];
 };
 
 // Do not edit below this line
